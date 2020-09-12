2f91fb5f5e11e1f3ba4577c94e62129a08216957
David <davidobob@laposte.net>
2020-09-12 11:50:51 +0200

    little modif

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ db.once("open", () => {
       pusher.trigger("messages", "inserted", {
         name: messageDetails.name,
         message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
       });
     } else {
       console.log("Pusher error");
