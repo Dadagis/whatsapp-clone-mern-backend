b65d23642645c05d4610f74470fccdf01a3ff571
David <davidobob@laposte.net>
2020-09-20 09:52:02 +0200

    added auth controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,39 +1,7 @@
-const _ = require("lodash");
-const bcrypt = require("bcrypt");
-const Joi = require("joi");
-const { User } = require("../models/user");
+const Auth = require("../controllers/auth_controller");
 const express = require("express");
 const router = express.Router();
 
-const validate = (req) => {
-  const schema = Joi.object({
-    email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max(1024).required(),
-  });
-
-  return schema.validate(req);
-};
-
-router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
-
-  if (error) {
-    res
-      .status(400)
-      .send(
-        `Expected ${error.details[0].message} ; was "${error.details[0].context.value}"`
-      );
-    return;
-  }
-
-  let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("Invalid email or password");
-
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) return res.status(400).send("Invalid email or password");
-
-  const token = user.generateAuthToken();
-  res.send(token);
-});
+router.post("/", Auth.login);
 
 module.exports = router;
