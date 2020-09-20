a0f35b585fed71cb386ad6a9164b0328af27b2ad
David <davidobob@laposte.net>
2020-09-20 10:45:08 +0200

    added user create login and me routes which are working

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const auth = require("../routes/auth");
+const users = require("../routes/users");
 const messages = require("../routes/messages");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/auth", auth);
+  app.use("/api/users", users);
   app.use("/api/messages", messages);
 };
