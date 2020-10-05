f57088320a0dc2219a4a9a0cf4e13f5128dfa07d
David <davidobob@laposte.net>
2020-10-05 18:59:58 +0200

    routing

--- routes/conversations.js
+++ routes/conversations.js
@@ -1,8 +1,9 @@
 const auth = require("../middlewares/auth");
 const express = require("express");
+const Conversation = require("../controllers/conversations_controller");
 const router = express.Router();
 
-router.get("/", auth);
-router.post("/", auth);
+router.get("/", auth, Conversation.findByUserId);
+router.post("/", auth, Conversation.create);
 
 module.exports = router;
